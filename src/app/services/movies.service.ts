dcc18d35c64547545a55edc2b86ebf0d673ca2dc
Mo Sharif <mo@mosh-media.com>
2020-01-17 23:12:15 -0800

    Update movies.service.ts

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,7 +1,10 @@
 import { environment } from "../../environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { IMovies } from "../models/movies";
+import { Movies, Movie } from "../models/movies";
 import { Injectable } from "@angular/core";
+import { shareReplay, take } from "rxjs/operators";
+import { from } from "rxjs";
+import { MovieDetails } from '../models/movie-details';
 
 @Injectable({
   providedIn: "root"
@@ -10,6 +13,14 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
 
   public getMovies = () => {
-    return this.http.get<IMovies>(`http://omdbapi.com/?s=Batman&apikey=e9ee62ed`)
+    return this.http
+      .get<Movies>(environment.moviesApi)
+      .pipe(shareReplay(), take(1));
+  };
+
+  public getMovieDetails = imdbID => {
+    return this.http
+      .get<MovieDetails>(`${environment.baseApi}&i=${imdbID}`)
+      .pipe(shareReplay(), take(1));
   };
 }
