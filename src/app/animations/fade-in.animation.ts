845a5af78b63586ce27e095fc11b86fc0afb3f29
Mo Sharif <mo@mosh-media.com>
2020-01-18 12:19:53 -0800

    Fix animation null property in prod

--- src/app/animations/fade-in.animation.ts
+++ src/app/animations/fade-in.animation.ts
@@ -6,12 +6,10 @@ import {
   transition
 } from "@angular/animations";
 
-const fadeIn = trigger("fadeIn", [
+export const fadeIn = trigger("fadeIn", [
   state("in", style({ opacity: 1 })),
 
   transition(":enter", [style({ opacity: 0 }), animate(600)]),
 
   transition(":leave", animate(600, style({ opacity: 0 })))
 ]);
-
-export default fadeIn;
