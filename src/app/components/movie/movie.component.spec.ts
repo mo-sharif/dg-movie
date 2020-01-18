a3e8cb3c690a41bbc2aa63eaaf5828d8861d569d
Mo Sharif <mo@mosh-media.com>
2020-01-18 11:38:01 -0800

    Update test code

--- src/app/components/movie/movie.component.spec.ts
+++ src/app/components/movie/movie.component.spec.ts
@@ -1,25 +1,39 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
-import { MovieComponent } from "./movie.component";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Movie } from "src/app/models/movies";
+import { Observable, of as observableOf, throwError } from "rxjs";
 import { MoviesService } from "src/app/services/movies.service";
+import { HttpClientModule } from "@angular/common/http";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { MovieComponent } from "./movie.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+class MockMyService {
+  public movie = { test: "test" };
+  public getMovieDetails(): Observable<any> {
+    return observableOf(this.movie);
+  }
+}
 
 describe("MovieComponent", () => {
   let component: MovieComponent;
   let fixture: ComponentFixture<MovieComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [MovieComponent],
-      imports: [HttpClientTestingModule],
-      providers: [MoviesService]
-    }).compileComponents();
-  }));
+  let moviesService: MoviesService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MovieComponent],
+      providers: [
+        {
+          provide: MoviesService,
+          useClass: MockMyService
+        }
+      ],
+      imports: [HttpClientModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(MovieComponent);
     component = fixture.componentInstance;
+    moviesService = TestBed.get(MoviesService);
     fixture.detectChanges();
   });
-
 });
